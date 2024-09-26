df524775d6e5c8d53c616eb7b43ddf159c09a5fd
sachinkhandare844@gmail.com <ksachin@cdac.in>
2024-09-26 14:22:11 +0530

    changes

--- Practice/src/Test/ConvertStringToInt.java
+++ Practice/src/Test/ConvertStringToInt.java
@@ -3,22 +3,78 @@ package Test;
 public class ConvertStringToInt {
 
 	public static void main(String[] args) {
-		String str = "123";
+		String str = "1337c0d3";
 		int result = StringToInt(str);
-		System.out.println("Integer:"+result);
+		System.out.println("Integer:" + result);
 
 	}
 
-	private static int StringToInt(String str) {
-		try {
-			return Integer.parseInt(str);
-		}
-		catch(NumberFormatException ex)
+	private static int StringToInt(String s) {
+		int i=0,n=s.length();
+		while(i< n && s.charAt(i)==' ')
 		{
-			throw new IllegalArgumentException("invalid Integer Format");
+			i++;
+		}
+		int sign=1;
+		while(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-'))
+		{
+			sign=(s.charAt(i)=='-') ? -1 :1;
+		}
+		int result=0;
+		while(i< n && Character.isDigit(s.charAt(i)))
+		{
+			int digit = s.charAt(i)-'0';
+			
+			if(result >(Integer.MAX_VALUE-digit)/10)
+			{
+				return (sign==1)? Integer.MAX_VALUE:Integer.MIN_VALUE;
+			}
+			result=result*10+digit;
+			i++;
 		}
 		
 		
+		return result*sign;
 	}
+
+//	private static int StringToInt(String s) {
+//		try {
+//			return Integer.parseInt(str);
+//		}
+//		catch(NumberFormatException ex)
+//		{
+//			throw new IllegalArgumentException("invalid Integer Format");
+//		}
+
+		// Step 1: Ignore leading whitespaces
+//		int i = 0, n = s.length();
+//		while (i < n && s.charAt(i) == ' ') {
+//			i++;
+//		}
+//
+//		// Step 2: Check for optional sign
+//		int sign = 1;
+//		if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+//			sign = (s.charAt(i) == '-') ? -1 : 1;
+//			i++;
+//		}
+//
+//		// Step 3: Convert the digits to an integer
+//		int result = 0;
+//		while (i < n && Character.isDigit(s.charAt(i))) {
+//			int digit = s.charAt(i) - '0';
+//
+//			// Step 4: Handle overflow/underflow
+//			if (result > (Integer.MAX_VALUE - digit) / 10) {
+//				return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+//			}
+//
+//			result = result * 10 + digit;
+//			i++;
+//		}
+//
+//		// Step 5: Return the final integer value with the sign
+//		return result * sign;
+//	}
 
 }
--- Practice/src/Test/ReverseNumber.java
+++ Practice/src/Test/ReverseNumber.java
@@ -3,15 +3,40 @@ package Test;
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-	 int num =12345;
-	 int rev=0 ,temp;
-	 while(num>0)
-	 {
-		 temp =num%10;
-		 rev=rev*10+temp;
-		 num=num/10;
-	 }
-       System.out.println("reverse:"+rev);
+//	 int num =12345;
+//	 int rev=0 ,temp;
+//	 while(num>0)
+//	 {
+//		 temp =num%10;
+//		 rev=rev*10+temp;
+//		 num=num/10;
+//	 }
+//       System.out.println("reverse:"+rev);
+//	}
+
+		Long num =-1534236469L;
+		Long reverse = reverse(num);
+		System.out.println(reverse);
+}
+
+	private static Long reverse(Long num) {
+		Long temp,rev=0L;
+		while(num>0 || num<0)
+		{
+			temp=num%10;
+			rev=rev*10+temp;
+			num=num/10;
+		}
+		return rev;
 	}
 
+//	  int reversed = 0;
+//      while (x != 0) {
+//          int digit = x % 10;
+//          x /= 10;
+//          if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+//          if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+//          reversed = reversed * 10 + digit;
+//      }
+//      return reversed;   
 }
